9b5f8636fafd7ddf1644dbf8515a95756a9488f1
Cory Moore <cory@corymoore.com>
2023-05-11 17:48:02 -0500

    iframe is no good for my purposes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/un6ZyFkqFKo"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
+      <h1>Cory Moore</h1>
     </>
   );
 }
