2ca321935b80e20ed382fd0f25ab760b3c57b131
Cory Moore <cory@corymoore.com>
2023-05-11 15:11:02 -0500

    testing video embed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Welcome to</h1>
-      <h3>CoryMoore.Com</h3>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/un6ZyFkqFKo"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </>
   );
 }
